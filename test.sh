abc19d50b7c6af0b5263bc57acf62e549c3ac605
Tatu Wikman <tatu.wikman@gmail.com>
2022-11-03 18:13:22 +0200

    seems to work

--- test.sh
+++ test.sh
@@ -7,3 +7,15 @@ for i in {1..10}
 do
   curl -s -X POST --header "Authorization: Bearer syt_UsernameB64_rndBase64_chksumB64" 'http://localhost:8080/' |grep 'Hostname'
 done
+
+# and 10 more with data changed around the username
+for i in {1..10}
+do
+  curl -s -X POST --header "Authorization: Bearer syt_UsernameB64_random64_checksum" 'http://localhost:8080/' |grep 'Hostname'
+done
+
+# and another 10 with different header
+for i in {1..10}
+do
+  curl -s -X POST --header "Authorization: Bearer syt_AnotherB64_rndBase64_chksumB64" 'http://localhost:8080/' |grep 'Hostname'
+done
